b1390684622301ac3626966374386417de628672
Gunal-k <gunalk.0007@gmail.com>
2025-03-07 11:03:00 +0530

    Refactor bitwise AND function for improved readability and change output to print

--- Day29-Bitwise_AND.py
+++ Day29-Bitwise_AND.py
@@ -1,16 +1,15 @@
 def bitwiseAnd(N, K):
     max_val = 0
-    for i in range(N-1,0,-1):
-        for j in range(N,i,-1):
-            val = i&j
+    for i in range(N-1, 0, -1):
+        for j in range(N, i, -1):
+            val = i & j
             if val < K:
-                max_val = max(val,max_val)
+                max_val = max(val, max_val)
                 if max_val == K-1:
                     return max_val
     return max_val
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
     t = int(input().strip())
 
     for t_itr in range(t):
@@ -18,6 +17,4 @@ if __name__ == '__main__':
         count = int(first_multiple_input[0])
         lim = int(first_multiple_input[1])
         res = bitwiseAnd(count, lim)
-        fptr.write(str(res) + '\n')
-
-    fptr.close()
+        print(res)
